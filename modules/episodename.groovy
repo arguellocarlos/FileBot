14b9073c9a48f782ff06521965f2f8e539f6f159
Carlos Arguello <84431643+arguellocarlos@users.noreply.github.com>
2025-08-13 01:14:35 -0500

    Restore episode name

--- modules/episodename.groovy
+++ modules/episodename.groovy
@@ -1,12 +1,12 @@
 {" - "}{s00e00}
 {
     def invalid_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
-    def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), '_')
+    def fixed_name = t.replaceAll(invalid_chars.collect { "\\" + it }.join('|'), ' -')
     fixed_name = fixed_name.length() > 100 ? " " : " - $fixed_name "
     def epnum = any { e } { special }
     def def_ep = "Episode $epnum"
     // if fixed_name contains default episode name, blank it out
     def epname = fixed_name.contains(def_ep) ? '' : fixed_name
-    // epname != '' ? epname : ''
-    ''
+    epname != '' ? epname : ''
+    // ''
 }
